277299a2fd03a2f7111c9c68d49865cbf7c38043
dir-FY24ogawarin <rin.ogawa.h226@dirbato.co.jp>
2024-04-22 16:34:23 +0900

    存在しないidに対する処理を追加

--- message_board/src/controllers/EditServlet.java
+++ message_board/src/controllers/EditServlet.java
@@ -31,7 +31,10 @@ public class EditServlet extends HttpServlet {
         request.setAttribute("_token", request.getSession().getId());
 
         // メッセージIDをセッションスコープに登録
+        //メッセージデータが存在しているときのみ
+        if(m != null) {
         request.getSession().setAttribute("message_id", m.getId());
+        }
 
         var rd = request.getRequestDispatcher("/WEB-INF/views/messages/edit.jsp");
         rd.forward(request, response);
