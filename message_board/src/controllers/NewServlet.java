f9669cc5d08de8f2301ce209a151eb464327ca18
dir-FY24ogawarin <rin.ogawa.h226@dirbato.co.jp>
2024-04-22 15:16:36 +0900

    CreateServlet.javaを作成

--- message_board/src/controllers/NewServlet.java
+++ message_board/src/controllers/NewServlet.java
@@ -1,34 +1,24 @@
 package controllers;
 
 import java.io.IOException;
-import java.sql.Timestamp;
-import javax.persistence.EntityManager;
-import models.Message;
-import utils.DBUtil;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import models.Message;
 
-/**
- * Servlet implementation class NewServlet
- */
 @WebServlet("/new")
 public class NewServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
+    private static final long serialVersionUID = 1L;
+
     /**
      * @see HttpServlet#HttpServlet()
      */
     public NewServlet() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         // CSRF対策
